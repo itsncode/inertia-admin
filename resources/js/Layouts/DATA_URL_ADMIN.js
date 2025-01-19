0e19cec88200d9e95496767c881af2bc2b6beff2
itsncode <itasetiani1997@gmail.com>
2025-01-19 16:22:56 +0700

    Initial commit

---
+++ resources/js/Layouts/DATA_URL_ADMIN.js
@@ -0,0 +1,18 @@
+export const DATA_URL_ADMIN = [
+    {
+        name: "Dashboard",
+        path: "dashboard",
+    },
+    {
+        name: "Chirps Management",
+        path: "dashboard.admin.chirps.index",
+    },
+    {
+        name: "Report management",
+        path: "dashboard.admin.report.index",
+    },
+    {
+        name: "Users Management",
+        path: "dashboard.admin.users.index",
+    },
+];
